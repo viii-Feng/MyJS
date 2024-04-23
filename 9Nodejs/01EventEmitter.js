fee7ee6f3726431fe88210b4d67045b4b78e7eea
vivi <2427418890@qq.com>
2024-04-23 17:33:10 +0800

    EventEmitter

---
+++ 9Nodejs/01EventEmitter.js
@@ -0,0 +1,61 @@
+//EventEmitter就是Node实现事件驱动的基础
+//在EventEmitter的基础上，Node几乎所有的模块都继承了这个类，这些模块拥有了自己的事件，可以绑定／触发监听器，实现了异步操作
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    on(type, handler) {
+        if (!this.events[type]) {
+            this.events[type] = [];
+        }
+        this.events[type].push(handler);
+    }
+
+    addListener(type,handler){
+        this.on(type,handler)
+    }
+
+    prependListener(type, handler) {
+        if (!this.events[type]) {
+            this.events[type] = [];
+        }
+        this.events[type].unshift(handler);
+    }
+
+    removeListener(type, handler) {
+        if (!this.events[type]) {
+            return;
+        }
+        this.events[type] = this.events[type].filter(item => item !== handler);
+    }
+
+    off(type,handler){
+        this.removeListener(type,handler)
+    }
+
+    emit(type, ...args) {
+        this.events[type].forEach((item) => {
+            Reflect.apply(item, this, args);
+        });
+    }
+
+    once(type, handler) {
+        this.on(type, this._onceWrap(type, handler, this));
+    }
+
+    _onceWrap(type, handler, target) {
+        const state = { fired: false, handler, type , target};
+        const wrapFn = this._onceWrapper.bind(state);
+        state.wrapFn = wrapFn;
+        return wrapFn;
+    }
+
+    _onceWrapper(...args) {
+        if (!this.fired) {
+            this.fired = true;
+            Reflect.apply(this.handler, this.target, args);
+            this.target.off(this.type, this.wrapFn);
+        }
+    }
+}
